f357fd86c87ff03a53170faf8d92299edee00013
XiomaraSM <sanchezmoreno@gmail.com>
2023-02-23 12:06:43 -0500

    punto de guardado documentacion con swagger

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -4,6 +4,213 @@ const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
+
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the workout to retrieve
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     tags:
+ *       - Workouts
+ *     requestBody:
+ *       description: New workout object
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/NewWorkout"
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Workout"
+ *       4XX:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "BAD_REQUEST"
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid request body"
+ */
+/**
+ * @openapi
+ *   patch:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Workout id
+ *       - in: body
+ *         name: changes
+ *         schema:
+ *           $ref: "#/components/schemas/WorkoutChanges"
+ *         required: true
+ *         description: Workout changes object
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Workout"
+ *       4XX:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "BAD_REQUEST"
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid request body"
+ */
+/**
+ * @openapi
+ *   delete:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Workout id
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       4XX:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "BAD_REQUEST"
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid request parameters"
+ */
+
 router
   .get("/", workoutController.getAllWorkouts)
   .get("/:workoutId", workoutController.getOneWorkout)
